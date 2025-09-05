c24b15a23c9c685bcb47058a6b8a0eb147042ddc
ideepak01 <60172019+ideepak01@users.noreply.github.com>
2025-09-05 20:22:20 +0530

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,28 +11,4 @@ import { RouterOutlet } from '@angular/router';
 
 export class AppComponent {
   title = 'img_capture_demo';
-  previewUrl = "";
-  onFileSelected(event: Event) {
-  const input = event.target as HTMLInputElement;
-
-  if (input.files && input.files[0]) {
-    const file = input.files[0];
-    console.log('Captured File:', file);
-
-    // Preview the image (optional)
-    const reader = new FileReader();
-    reader.onload = (e: any) => {
-      this.previewUrl = e.target.result; // Bind this to <img> in template
-    };
-    reader.readAsDataURL(file);
-
-    // TODO: Upload file to your backend here
-    const formData = new FormData();
-    formData.append('photo', file);
-
-    // Example: HTTP upload
-    // this.http.post('/api/upload', formData).subscribe(res => console.log(res));
-  }
-}
-
 }
